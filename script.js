c4a7ec928f1645f2016f517dfdc34113b5c7a40a
Kristin <kutsiepudy3841@gmail.com>
2026-09-01 17:49:21 -0700

    Add toggle functionality for buttons

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelectAll('.toggle-btn').forEach(button => {
+  button.addEventListener('click', () => {
+    const targetID = button.getAttribute('data-target');
+    const targetDIV = document.getElementById(targetID);
+    targetDIV.classList.toggle('hidden');
+  };
+});
