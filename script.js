f5efb545741e474b6067ff77601449f769afa86d
Kristin <kutsiepudy3841@gmail.com>
2026-09-01 18:12:23 -0700

    Fix typo in document.querySelectorAll method

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.querySelectAll('.toggle-btn').forEach(button => {
+document.querySelectorAll('.toggle-btn').forEach(button => {
   button.addEventListener('click', () => {
     const targetID = button.getAttribute('data-target');
     const targetDIV = document.getElementById(targetID);
