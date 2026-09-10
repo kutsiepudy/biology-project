dfce62126522c7d7737576a97ba81c8bcf8d170d
Kristin <kutsiepudy3841@gmail.com>
2026-09-09 20:30:40 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const cursor = document.getElementById('aquaCursor');
 const cursorAsset = document.getElementById('cursorAsset');
 
 const idle = "cursor/aquaIdle.gif";
-const hover = "cursor/aquaCurious.png";
+const hover = "cursor/aquaHappy.png";
 const click = "cursor/aquaWave.gif";
 
 let isHoveringLink = false;
