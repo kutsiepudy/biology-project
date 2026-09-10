de7ae59d06b0752b53ae145b4d3476d642b061d0
Kristin <kutsiepudy3841@gmail.com>
2026-09-10 09:26:53 -0700

    Update cursor asset to use idle image
    Change cursor asset to idle when not hovering or clicking.

--- script.js
+++ script.js
@@ -4,14 +4,13 @@ const toggleButtons = document.querySelectorAll('.toggle-btn');
 const idle = "cursor/aquaIdle.gif";
 const hover = "cursor/aquaHappy.gif";
 const click = "cursor/aquaClap.gif";
-const move = "cursor/aquaHappy.gif"
 
 let isHoveringLink = false;
 
 document.addEventListener('mousemove', (e) => {
   cursor.style.transform = `translate3d(${e.clientX}px, ${e.clientY}px, 0)`;
   if (!isHoveringLink && !document.querySelector(".cursor-container.is-clicking")) {
-    cursorAsset.src = move;
+    cursorAsset.src = idle;
   }
 });
 
